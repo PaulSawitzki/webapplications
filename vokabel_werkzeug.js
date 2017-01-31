4a45b58f70b658a70cb4f07ee838415fc4e5296f
Rainer Sawitzki <training@rainer-sawitzki>
2017-01-31 20:03:20 +0100

    bugfix

--- vokabel_werkzeug.js
+++ vokabel_werkzeug.js
@@ -5,8 +5,8 @@ var vokabeln = [];
 for(i in array) {
     var line = array[i];
     if (line && line.trim().length > 0){
-      var deutsch_fremdsprache = line.split('=')
-      var vokabel = {deutsch: deutsch_fremdsprache[1], fremdsprache: deutsch_fremdsprache[0]};
+      var deutsch_fremdsprache = line.split(':')
+      var vokabel = {deutsch: deutsch_fremdsprache[0], fremdsprache: deutsch_fremdsprache[1]};
       vokabeln.push(vokabel)
     }
 

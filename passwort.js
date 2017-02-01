0cb39895499242f4a8458eb6cd93a2034dfb546e
Paul Sawitzki <paulsawitzki@gmail.com>
2017-02-01 20:35:44 +0100

    Neue Vokabeln + Passwort

---
+++ passwort.js
@@ -0,0 +1,25 @@
+function programm(){
+  werkzeug("#lstücke").hide();
+  werkzeug("#bereich1").hide();
+  var enterKeyPressed = function(e) {
+  var key = e.which;
+  if(key == 13){
+    onKeyPress()
+ }
+}
+werkzeug('#Textfeld').keypress(enterKeyPressed);
+}
+
+function onKeyPress(){
+//  werkzeug("#Wort").hide();
+  var Wort = werkzeug("#Textfeld").val();
+  if(Wort == "8/#swag/B"){
+    werkzeug("#lstücke").show();
+    werkzeug("#bereich1").hide();
+  }
+  else{
+    werkzeug("#bereich1").show();
+    werkzeug("#lstücke").hide();
+  }
+}
+werkzeug.start(programm)
